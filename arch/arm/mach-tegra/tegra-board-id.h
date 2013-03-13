f679f611d0fb2faf17ed6c8443ab2a34be6ae11e
Matt Wagner <mwagner@nvidia.com>
2013-03-13 15:46:54 -0700

    ARM: Tegra: Rearrange Boards and add P2560
    Bug 1247326
    
    Change-Id: Ia3efba1eb23bcff6ed999824ed0494a3e854dc96
    Signed-off-by: Matt Wagner <mwagner@nvidia.com>
    Reviewed-on: http://git-master/r/209002
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra-board-id.h
+++ arch/arm/mach-tegra/tegra-board-id.h
@@ -22,20 +22,21 @@
 #define _MACH_TEGRA_BOARD_ID_H
 
 /* Processor Board  ID */
+#define BOARD_E1545   0x0609
+#define BOARD_E1569   0x0621
+#define BOARD_E1575   0x0627
+#define BOARD_E1577   0x0629
+#define BOARD_E1580   0x062C
+#define BOARD_E1582   0x062E
 #define BOARD_E1611   0x064B
 #define BOARD_E1612   0x064C
 #define BOARD_E1613   0x064D
 #define BOARD_E1614   0x064E
 #define BOARD_E1641   0x0669
-#define BOARD_E1580   0x062C
-#define BOARD_E1575   0x0627
-#define BOARD_E1577   0x0629
-#define BOARD_P2454   0x0996
-#define BOARD_E1582   0x062E
-#define BOARD_E1545   0x0609
-#define BOARD_E1569   0x0621
 #define BOARD_P1640   0x0668
 #define BOARD_P1988   0x07C4
+#define BOARD_P2454   0x0996
+#define BOARD_P2560   0x0A00
 
 /* Board Fab version */
 #define BOARD_FAB_A00			0x0
