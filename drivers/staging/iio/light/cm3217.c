b3d6d1a5b0ad07d11ecdfca7c090f64f1e2a587d
Sri Krishna chowdary <schowdary@nvidia.com>
2013-07-12 18:46:15 +0530

    staging: iio: cm3217: Add regulator logic
    Cm3217 assumes regulator is always on and hence
    did not earlier have any regulator on/off logic. This
    caused issues when regulator is turned off without
    driver's knowledge. Hence, add regulator enable/disable
    logic.
    
    Bug 1324000
    
    Change-Id: Iec6fb73e81eab088daf0e153ec2bc94a9c53511d
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/248357
    (cherry picked from commit b35f33c482cbe34afa7071f2fecc0fad11ff1c27)
    Reviewed-on: http://git-master/r/251759
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/staging/iio/light/cm3217.c
+++ drivers/staging/iio/light/cm3217.c
@@ -357,6 +357,8 @@ static int lightsensor_enable(struct cm3217_info *lpi)
 	int ret = 0;
 	uint8_t cmd = 0;
 
+	als_power(1);
+
 	mutex_lock(&lpi->enable_lock);
 
 	if (lpi->als_enable)
@@ -411,6 +413,8 @@ static int lightsensor_disable(struct cm3217_info *lpi)
 
 out:
 	mutex_unlock(&lpi->disable_lock);
+
+	als_power(0);
 
 	return ret;
 }
