06143fb20982d7b36532abfa97ca563724e4e268
Arto Merilainen <amerilainen@nvidia.com>
2013-10-01 15:33:24 +0300

    video: tegra: host: Fix race in gr3d scaling
    The scaling code used cancel_work_sync() while holding a mutex. As the work
    itself uses the same mutex, we risk causing a deadlock.
    
    This patch refactors the code so that the mutex is not hold while calling
    cancel_work_sync().
    
    Bug 1371500
    
    Change-Id: I3aa0de168cebcc1d8d1843813caee5e82fe3df06
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/280730
    (cherry picked from commit 763125fba1c9a8a67a9968c8502c17465665eb35)
    Reviewed-on: http://git-master/r/354208
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Gabby Lee <galee@nvidia.com>
    Reviewed-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- drivers/video/tegra/host/gr3d/pod_scaling.c
+++ drivers/video/tegra/host/gr3d/pod_scaling.c
@@ -3,7 +3,7 @@
  *
  * Tegra Graphics Host 3D clock scaling
  *
- * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
