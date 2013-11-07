bd9d8a8d575d65a96b62b60f308eca081654b990
Ahung Cheng <ahcheng@nvidia.com>
2013-11-07 15:44:57 +0800

    video: tegra: host: Fix memory leakage in msenc
    Add kfree for msenc in deinit
    
    bug 1402255
    
    (Cherry-picked from commit bd1fc938268f40581c2ff7f6f698bdd7c8c8bc2a)
    Reviewed-on: http://git-master/r/327535
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    
    Change-Id: I8b921d2b221126097ebf48e14c679b0e08245bed
    Signed-off-by: Ahung Cheng <ahcheng@nvidia.com>
    Reviewed-on: http://git-master/r/353527
    Reviewed-by: Harry Hong <hhong@nvidia.com>
    Tested-by: Harry Hong <hhong@nvidia.com>

--- drivers/video/tegra/host/msenc/msenc.c
+++ drivers/video/tegra/host/msenc/msenc.c
@@ -3,7 +3,7 @@
  *
  * Tegra MSENC Module Support
  *
- * Copyright (c) 2012-2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
