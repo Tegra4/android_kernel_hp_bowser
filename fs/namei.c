67e34fa7120fbc195eb852897f12a2b25bddbe4b
c457 <android.c357@gmail.com>
2016-12-07 10:37:16 -0600

    bowser: fix misc warnings
    * These lead to errors

--- fs/namei.c
+++ fs/namei.c
@@ -2861,7 +2861,7 @@ EXPORT_SYMBOL(vfs_rmdir);
 static long do_rmdir(int dfd, const char __user *pathname)
 {
 	int error = 0;
-	char * name;
+	char * name = 0;
 	struct dentry *dentry;
 	struct nameidata nd;
 
@@ -2964,7 +2964,7 @@ EXPORT_SYMBOL(vfs_unlink);
 static long do_unlinkat(int dfd, const char __user *pathname)
 {
 	int error;
-	char *name;
+	char *name = 0;
 	struct dentry *dentry;
 	struct nameidata nd;
 	struct inode *inode = NULL;
@@ -3063,7 +3063,7 @@ SYSCALL_DEFINE3(symlinkat, const char __user *, oldname,
 		int, newdfd, const char __user *, newname)
 {
 	int error;
-	char *from;
+	char *from = 0;
 	struct dentry *dentry;
 	struct path path;
 
